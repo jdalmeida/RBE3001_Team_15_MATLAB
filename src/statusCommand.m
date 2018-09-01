a946a661b2880dee0baec66a14684397d6f78ff9
Jesse Fransisco d'Almeida <jfdalmeida@ak120d-05.wpi.edu>
2018-09-01 15:36:43 -0400

    Created status command and got home position data

--- src/statusCommand.m
+++ src/statusCommand.m
@@ -31,14 +31,22 @@ myHIDSimplePacketComs.connect();
 % Create a PacketProcessor object to send data to the nucleo firmware
 
 pp = PacketProcessor(myHIDSimplePacketComs);
-
+file='calibrations.csv';
+calibrationCount=100;
 try
   SERV_ID = 36;            % we will be talking to server ID 37 on
                            % the Nucleo
 
   DEBUG   = true;          % enables/disables debug prints
-    
-  while(1)
+  
+  %Runs the first time to get rid of the all zeros issue
+  packet = zeros(15, 1, 'single');
+  runningAverage=zeros(1,3,'single');
+  % Send packet to the server and get the response
+  returnPacket = pp.command(SERV_ID, packet);
+  pause(.5);
+  fclose(fopen(file, 'w'));
+  for i=1:calibrationCount
       % Instantiate a packet - the following instruction allocates 64
       % bytes for this purpose. Recall that the HID interface supports
       % packet sizes up to 64 bytes.
@@ -47,13 +55,14 @@ try
       % Send packet to the server and get the response
       returnPacket = pp.command(SERV_ID, packet);
       r = returnPacket';
-      
+
       disp('Received Packet:');
       disp(r([1, 4, 7]));
-      
-      pause(.5);
+      runningAverage=runningAverage+r([1,4,7]);
+      dlmwrite(file,r([1,4,7]),'-append');
   end
-
+  runningAverage=runningAverage/calibrationCount
+   dlmwrite(file,runningAverage,'-append');
 catch exception
     getReport(exception);
     disp('Exited on error, clean shutdown');
