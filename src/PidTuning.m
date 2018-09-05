d99dcf76949fe2ae13a04b2df082d6de66c648e9
Jesse Fransisco d'Almeida <jfdalmeida@ak120d-11.wpi.edu>
2018-09-05 13:44:51 -0400

    attempts new pid tuning for more data

--- src/PidTuning.m
+++ src/PidTuning.m
@@ -6,14 +6,15 @@ pp = PacketProcessor(myHIDSimplePacketComs);
 %for link 2, pid of .0025, 0, .028 works well +/-3deg
 %for link 3, pid of .0025, 0 , .02
 %% Sets the pid values for each of the arms before moving to the setpoint
+% for pid2 i = .01
 jToSet1=zeros(3,1,'single');
-jToSet1=[.0025 0 0];
+jToSet1=[.0025 .01 0];
 jToSet2=zeros(3,1,'single');
-jToSet2=[.0025 0 .028];
+jToSet2=[.0025 .01 .028];
 jToSet3=zeros(3,1,'single');
-jToSet3=[.0025 0 .02];
+jToSet3=[.0025 .01 .02];
 PIDConfig(pp, jToSet1, jToSet2, jToSet3);
 %% Moves the robot to the setpoint (in degrees) for each of the joints.
-Setpoint(pp,-20,30.0132,5);
+Setpoint(pp,30,50,50);
 pp.shutdown();
 clear
