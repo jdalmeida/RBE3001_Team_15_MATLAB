15472a04a700f86465880cc71bb600d50c1d614f
Jesse Fransisco d'Almeida <jfdalmeida@ak120d-11.wpi.edu>
2018-09-06 09:03:42 -0400

    ?

--- src/PidTuning.m
+++ src/PidTuning.m
@@ -8,13 +8,15 @@ pp = PacketProcessor(myHIDSimplePacketComs);
 %% Sets the pid values for each of the arms before moving to the setpoint
 % for pid2 i = .01
 jToSet1=zeros(3,1,'single');
-jToSet1=[.0025 .01 0];
+jToSet1=[0.3 0 0.25];
 jToSet2=zeros(3,1,'single');
-jToSet2=[.0025 .01 .028];
+jToSet2=[0.3 0 0.25];
 jToSet3=zeros(3,1,'single');
-jToSet3=[.0025 .01 .02];
+jToSet3=[0.3 0 0.25];
 PIDConfig(pp, jToSet1, jToSet2, jToSet3);
 %% Moves the robot to the setpoint (in degrees) for each of the joints.
-Setpoint(pp,30,50,50);
+Setpoint(pp,-20,13,20);
+% pause(2);
+% Setpoint(pp,0,0,0);
 pp.shutdown();
 clear
