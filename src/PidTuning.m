df307aba5602ccfade2856c4a67ec7fbeb77f856
Jesse Fransisco d'Almeida <jfdalmeida@ak120d-11.wpi.edu>
2018-09-06 09:10:44 -0400

    reset PID values

--- src/PidTuning.m
+++ src/PidTuning.m
@@ -8,12 +8,13 @@ pp = PacketProcessor(myHIDSimplePacketComs);
 %% Sets the pid values for each of the arms before moving to the setpoint
 % for pid2 i = .01
 jToSet1=zeros(3,1,'single');
-jToSet1=[0.3 0 0.25];
+jToSet1=[0.0025 0 0];
 jToSet2=zeros(3,1,'single');
-jToSet2=[0.3 0 0.25];
+jToSet2=[0.0025 0 0.28];
 jToSet3=zeros(3,1,'single');
-jToSet3=[0.3 0 0.25];
+jToSet3=[0.0025 0 0.2];
 PIDConfig(pp, jToSet1, jToSet2, jToSet3);
+
 %% Moves the robot to the setpoint (in degrees) for each of the joints.
 Setpoint(pp,-20,13,20);
 % pause(2);
