225b8f1c2ab90b9df09193b51c500f7534dba16a
Kent J Libby <klibby@ak120d-05.wpi.edu>
2018-09-15 17:15:04 -0400

    Gets all parts of lab working
    Minor issue with tuning (possibly)
    :

--- src/RequiredSteps/PidTuning.m
+++ src/RequiredSteps/PidTuning.m
@@ -13,11 +13,11 @@ jToSet2=zeros(3,1,'single');
 jToSet2=[.0028 0 .028];
 jToSet3=zeros(3,1,'single');
 jToSet3=[.0025 0 .02];
-% PIDConfig(pp, jToSet1, jToSet2, jToSet3);
+PIDConfig(pp, jToSet1, jToSet2, jToSet3);
 
 %% Moves the robot to the setpoint (in degrees) for each of the joints.
 Setpoint(pp,10,40,20);
-% pause(2);
-% Setpoint(pp,0,0,0);
+pause(2);
+Setpoint(pp,-60,25,0);
 pp.shutdown();
 clear
--- src/RequiredSteps/lab3/TrajGenStep7.m
+++ src/RequiredSteps/lab3/TrajGenStep7.m
@@ -7,7 +7,7 @@ fileName=writer.BeginCsv('step7QuinticTriangle');
 
 PID1=[.0025 0 0];
 PID2=[.0025 0 .028];
-PID3=[.0025 0 .02];
+PID3=[.002 0 .02];
 PIDConfig(pp, PID1, PID2, PID3);
 
 LivePlot3D([0,0,0], true);
@@ -38,35 +38,34 @@ conds = [t0, tf, q0, qf, v0, vf, a0, af];
 %creates 3xcond size for joints (joints = rows)
 condsMat = [conds; conds; conds];
 
-toffset = .5;    %difference between t0 and tf
+toffset = .75;    %difference between t0 and tf
 
-angles = zeros(1,3, 'single'); % to hold end positions 
-
+posn = zeros(1,3, 'single'); % to hold end positions 
+angles = zeros(1,3, 'single'); 
 % Initial point is the 3rd vertex of the triangle
-prevPoint = Triangle(3, :);
+prevPos = Triangle(3, :);
 
 tic;
 
 % iterate through each setpoint in triangles
 for i = 0:3
-    %% Using end effector postion
     index = mod(i,3) + 1; % continuously cycle through points of the triangle
     
     % initial and final joint angles required for the trajectory
     % converts the tip position into angles
-    prevAngles = ikin(prevPoint);
-    setAngles = ikin(Triangle(index, :));
+%     prevAngles = ikin(prevPoint);
+%     setAngles = ikin(Triangle(index, :));
+    setPos = Triangle(index, :);
     
     now = toc;  % current timestamp  
     startTime = now; % to keep track of when the trajectory is starting
     
-    %% Working in joint angles
-    % iterates through each of the joints to get conditions
-    for joint = 1:3
-        condsMat(joint, 1) = startTime;           %t0
-        condsMat(joint, 2) = startTime + toffset; %tf
-        condsMat(joint, 3) = prevAngles(joint);   %q0
-        condsMat(joint, 4) = setAngles(joint);    %qf
+    % iterates through each of the axes to get conditions
+    for axis = 1:3
+        condsMat(axis, 1) = startTime;           %t0
+        condsMat(axis, 2) = startTime + toffset; %tf
+        condsMat(axis, 3) = prevPos(axis);   %q0
+        condsMat(axis, 4) = setPos(axis);    %qf
     end
     
     % loop across the trajectory
@@ -75,9 +74,9 @@ for i = 0:3
         
         % generate the positions for each joint
         for j = 1:3
-            angles(j) = QuinticTraj(now, condsMat(j, :));
+            posn(j) = QuinticTraj(now, condsMat(j, :)); %this is a 1x3 that stores x y z of tip position
         end
-        
+        angles = ikin(posn);
         % Go to the setpoint based on the equation
         Setpoint(pp, angles(1), angles(2), angles(3));
         
@@ -91,8 +90,8 @@ for i = 0:3
         writer.AppendCsv(fileName, data);
         
     end
-    
-    prevPoint=Triangle(index, :);
+    pause(.25);
+    prevPos=setPos;
 end
     
 % Clear up memory upon termination
