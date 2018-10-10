a1ab3635281ecf3014ef130c9aa10c37dcf9bbea
Jesse Fransisco d'Almeida <jfdalmeida@ak120d-11.wpi.edu>
2018-10-10 16:45:42 -0400

    k

--- src/Functions/Utility/Setpoint.m
+++ src/Functions/Utility/Setpoint.m
@@ -5,13 +5,15 @@ global PID_ID
 ANGLE_TO_TIC = 4096.0/360.0;
 
 try
+    curPos = GetCurrentPos(pp);
+    
+    
     packet = zeros(15, 1, 'single');
     packet(1) = ANGLE_TO_TIC * joint1;
     packet(4) = ANGLE_TO_TIC * joint2;
     packet(7) = ANGLE_TO_TIC * joint3;
     
     statusPacket = pp.command(PID_ID, packet);
-    
     
     angles = statusPacket([1,4,7]);
 catch exception
